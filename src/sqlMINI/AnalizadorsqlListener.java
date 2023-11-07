625af2733e36b0b03e5ecb8f878ef0ef7c9661a6
SSierraArk <ssierra@will-soft.net>
2023-11-08 00:25:05 +0100

    Renamed sqlMINI to sqlmini

--- src/sqlMINI/AnalizadorsqlListener.java
+++
@@ -1,9 +0,0 @@
-import org.antlr.v4.runtime.tree.TerminalNode;
-
-public class AnalizadorsqlListener extends sqlgParserBaseListener {
-
-    @Override
-    public void visitTerminal(TerminalNode node) {
-        System.out.print(node);
-    }
-}
